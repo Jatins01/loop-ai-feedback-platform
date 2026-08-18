12e8375009fd001efaf77cff63fd676d9d2b066c
Anthony Appiah Adjei <adjeianthony77@gmail.com>
2026-08-18 16:11:27 +0000

    Created layout and page in the app directory

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mphXpex+O3UYGe92IsoaRB73ULKAX+uuzwuN13sAw1g=\"\n}"
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <h1>Welcome to Loop AI Feedback Platform</h1>
+}
